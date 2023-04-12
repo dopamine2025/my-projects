0b1beec413cbd1face151763d9b482606f424640
dopamine2025 <128512034+dopamine2025@users.noreply.github.com>
2023-04-12 09:06:38 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import responses
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+def run_discord_bot():
+    masterkey = "MTA5MDk4MjQyOTE4MDgyMTU2OQ.GNtD-J.cbJ3FPOmKVFvqjGwjwt6BlFY5BoTUJnytOIz5U"
+    client = discord.Client()
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        print(f"{username} said: '{user_message}' ({channel})")
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+    client.run(masterkey)
---
+++ responses.py
@@ -0,0 +1,13 @@
+import random
+def handle_response (message):
+    p_message = message.lower()
+    if p_message == 'fuck':
+        return 'Swear words arent good stop using them'
+    if p_message == '!kick':
+        return 'try not to kick, kicking is a bad idea'
+    if p_message == 'nigga':
+        return 'rasicits comments arent allowed'
+    if p_message == '@ban':
+        return 'try not ban people it isnt really that funny 😑'
+    if p_message == 'gay':
+        return 'Homophobia isnt allowed'
